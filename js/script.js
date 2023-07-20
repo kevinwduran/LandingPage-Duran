5d5b69c59ae4dba8289dd09e1682b8c33c92f2ae
Kevin Duran <kevinwduran23@gmail.com>
2023-07-20 19:29:27 -0300

    Responsividade 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // slides:
 document.addEventListener('DOMContentLoaded', function() {
-    let header = document.querySelector('.header');
-    let tamanhoElemento = header.offsetHeight;
+    let header = document.querySelector('.header')
+    let tamanhoElemento = header.offsetHeight
   
-    let header_bg = document.querySelector('.header-bg');
-    header_bg.style.height = tamanhoElemento + 'px';
-    let introducao = document.querySelector('.introducao');
-    introducao.style.marginTop = tamanhoElemento + 'px';
+    let header_bg = document.querySelector('.header-bg')
+    header_bg.style.height = tamanhoElemento + 'px'
+    let introducao = document.querySelector('.introducao')
+    introducao.style.marginTop = tamanhoElemento + 'px'
   });
 
 let contador = 1;
-document.getElementById('radio1').checked = true;
+document.getElementById('radio1').checked = true
 setInterval(function(){
     nextImage();
 }, 3000)
@@ -18,9 +18,9 @@ setInterval(function(){
 function nextImage(){
     contador++;
     if(contador > 4){
-        contador = 1;
+        contador = 1
     }
-    document.getElementById('radio'+contador).checked = true;
+    document.getElementById('radio'+contador).checked = true
 }
 
 
@@ -40,3 +40,12 @@ function eventosGaleria(img){
 }
 
 galeria.forEach(eventosGaleria)
+
+// Animação menu abrir/fechar mobile
+let nav_ul = document.querySelector('.header-menu')
+function openMenu(){
+    nav_ul.classList.add('open')
+}
+function closeMenu(){
+    nav_ul.classList.remove('open')
+}
