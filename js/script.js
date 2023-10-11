d0a6bdeeab5b94523aa487577bc25fa0fe892a88
Kevin Duran <kevinwduran23@gmail.com>
2023-10-11 15:20:46 -0300

    Atualização subpáginas

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
-// slides:
-document.addEventListener('DOMContentLoaded', function() {
-    let header = document.querySelector('.header')
-    let tamanhoElemento = header.offsetHeight
-  
-    let header_bg = document.querySelector('.header-bg')
-    header_bg.style.height = tamanhoElemento + 'px'
-    let introducao = document.querySelector('.introducao')
-    introducao.style.marginTop = tamanhoElemento + 'px'
-  });
-
-let contador = 1;
-document.getElementById('radio1').checked = true
-setInterval(function(){
-    nextImage();
-}, 3000)
-
-function nextImage(){
-    contador++;
-    if(contador > 4){
-        contador = 1
-    }
-    document.getElementById('radio'+contador).checked = true
-}
-
-
+//galeria de fotos acionamentos
 const galeria = document.querySelectorAll('.acionamentos-imgs img')
 const galeriaContainer = document.querySelector('.acionamentos-imgs')
 
@@ -49,3 +24,31 @@ function openMenu(){
 function closeMenu(){
     nav_ul.classList.remove('open')
 }
+
+
+
+// slides:
+document.addEventListener('DOMContentLoaded', function() {
+    let header = document.querySelector('.header')
+    let tamanhoElemento = header.offsetHeight
+  
+    let header_bg = document.querySelector('.header-bg')
+    header_bg.style.height = tamanhoElemento + 'px'
+    let introducao = document.querySelector('.introducao')
+    introducao.style.marginTop = tamanhoElemento + 'px'
+  });
+
+let contador = 1;
+document.getElementById('radio1').checked = true
+setInterval(function(){
+    nextImage();
+}, 3000)
+
+function nextImage(){
+    contador++;
+    if(contador > 4){
+        contador = 1
+    }
+    document.getElementById('radio'+contador).checked = true
+}
+
