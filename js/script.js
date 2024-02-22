6dbc96f13b8c3ba59c6a29638b4b044cf5ddf6f6
Kevin Duran <kevinwduran23@gmail.com>
2024-02-22 20:23:48 -0300

    Criação página Double Vision

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-import SlidesAnimation from './modules/slides.js';
-import photoGalleryClick from './modules/photoGalleryAc.js';
-import changeContentControls from './modules/changeContentControls.js';
-import menuHamb from './modules/clickMenuHamb.js';
-import lazyLoading from './modules/lazyLoading.js';
+import SlidesAnimation from "./modules/slides.js";
+import photoGalleryClick from "./modules/photoGalleryAc.js";
+import changeContentControls from "./modules/changeContentControls.js";
+import menuHamb from "./modules/clickMenuHamb.js";
+import lazyLoading from "./modules/lazyLoading.js";
 
-const slidesAnimation = new SlidesAnimation('input[name="radio-btn"]', '.manual-navegacao label');
+const slidesAnimation = new SlidesAnimation(
+  'input[name="radio-btn"]',
+  ".manual-navegacao label",
+);
 slidesAnimation.init();
 
 photoGalleryClick();
@@ -13,29 +16,28 @@ menuHamb();
 lazyLoading();
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  let header = document.querySelector(".header");
 
-document.addEventListener('DOMContentLoaded', function () {
-    let header = document.querySelector('.header');
+  // Verifica se o elemento .header existe
+  if (header) {
+    let tamanhoElemento = header.offsetHeight;
 
-    // Verifica se o elemento .header existe
-    if (header) {
-        let tamanhoElemento = header.offsetHeight;
-
-        let header_bg = document.querySelector('.header-bg');
-        if (header_bg) {
-            header_bg.style.height = tamanhoElemento + 'px';
-        }
-
-        let introducao = document.querySelector('.introducao');
-        if (introducao) {
-            introducao.style.marginTop = tamanhoElemento + 'px';
-        }
+    let header_bg = document.querySelector(".header-bg");
+    if (header_bg) {
+      header_bg.style.height = tamanhoElemento + "px";
     }
+
+    let introducao = document.querySelector(".introducao");
+    if (introducao) {
+      introducao.style.marginTop = tamanhoElemento + "px";
+    }
+  }
 });
 
-import SlideNav from './modules/slide.js';
+import SlideNav from "./modules/slide.js";
 
-const slide = new SlideNav('.slide-wrapper', '.slide-big');
+const slide = new SlideNav(".slide-wrapper", ".slide-big");
 slide.init();
 
-slide.addControl('.custom-control');
+slide.addControl(".custom-control");
