f793ae6e73cbe6992fb6972af7436eb0d58bbb02
Kevin Duran <kevinwduran23@gmail.com>
2024-02-26 19:37:51 -0300

    Correção de layout e adição de vídeo

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,18 +1,17 @@
 // form.js
 export default function form() {
-  const contato = document.getElementById('contato');
+  const contato = document.getElementById("contato");
 
   function handleKeyUp(event) {
-     const target = event.target;
+    const target = event.target;
 
-     if (!target.checkValidity()) {
-        target.classList.add('invalido');
-        target.nextElementSibling.innerText = target.validationMessage;
-     } else if (target.checkValidity()) {
-        target.classList.remove('invalido');
-        target.nextElementSibling.innerText = '';
-     }
+    if (!target.checkValidity()) {
+      target.classList.add("invalido");
+      target.nextElementSibling.innerText = target.validationMessage;
+    } else if (target.checkValidity()) {
+      target.classList.remove("invalido");
+      target.nextElementSibling.innerText = "";
+    }
   }
-  contato.addEventListener('keyup', handleKeyUp);
+  contato.addEventListener("keyup", handleKeyUp);
 }
-
